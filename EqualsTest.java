d6d6e87524ad8d2cb303d5930613e4dfef12b641
Luis Gabriel Oliveira <97142989+luisgabriel1@users.noreply.github.com>
2022-12-30 15:19:12 -0300

    Update EqualsTest.java

--- EqualsTest.java
+++ EqualsTest.java
@@ -4,11 +4,11 @@ public class EqualsTest {
     public static void main(String[] args) {
         Equals u1 = new Equals();
 
-        u1.nome = "Luis Gabriel";
+        u1.name = "Luis Gabriel";
         u1.Gmail = "LuisGabriel@gmail.com";
 
         Equals u2 = new Equals();
-        u2.nome = "Luis Gabriel";
+        u2.name = "Luis Gabriel";
         u2.Gmail = "LuisGabriel@gmail.com";
 
         System.out.println(u1 == u2);
